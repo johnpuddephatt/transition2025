5dfd0559cfd777cfd97db6c1857276cff9a34f4e
John Puddephatt <john@jdp.org.uk>
2025-03-11 19:57:50 +0000

    debug

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,12 +5,19 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
+  @while (have_posts())
+    @php the_post() @endphp
     <div class="container">
       @include('partials.page-header', ['class' => 'entry-header__single-page'])
+
+      @if ($page->thumbnail)
+        <figure class="entry-header--image entry-header--image__page">
+          {!! $page->thumbnail !!}
+        </figure>
+      @endif
+
       @include('partials.content-page-about')
     </div>
   @endwhile
-    @include('partials.newsletter')
-
+  @include('partials.newsletter')
 @endsection
