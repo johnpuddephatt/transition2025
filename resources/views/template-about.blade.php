cef825b7f3770226b0cf0cce2158dd6c0b09e82e
John Puddephatt <john@jdp.org.uk>
2025-03-11 20:18:01 +0000

    test about

--- resources/views/template-about.blade.php
+++ resources/views/template-about.blade.php
@@ -3,12 +3,5 @@
 @extends('layouts.app')
 
 @section('content')
-  @while (have_posts())
-    @php the_post() @endphp
-    <div class="container">
-      @include('partials.page-header', ['class' => 'entry-header__single-page'])
-      @include('partials.content-page-about')
-    </div>
-  @endwhile
-  @include('partials.newsletter')
+  Test about
 @endsection
